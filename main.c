b7b230ab8b3d94dae78914ea6b2c8f2d1f518164
BayaABBACI <abbacibaya@gmail.com>
2020-01-12 12:54:35 +0100

    programme principal

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include "protos.h"
 
-int main()
+int main(int argc, char **argv)
     {
 
-     return 0;
+    if(argc<3)
+      {
+      return printf("Usage : [train ppm] [test ppm]\n"),-1;
+      }
+
+    ppm_t *train_images;
+    train_images = ppm_open(argv[1]); 
+    ppm_t *test_images;
+    test_images = ppm_open(argv[2]);     
+    trainer(1,train_images,test_images);
+    
+    if (!train_images & !test_images)
+      {
+      return printf("Error: cannot open ppm file (%s) (%s)\n", argv[1],     argv[2]), -1;
+      }
     }
 
 
