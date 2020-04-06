339f72127c9aafca6022e9f36a586e8e7fb489a1
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2020-04-06 12:31:55 +0200

    main du CNN

--- main.c
+++ main.c
@@ -1,45 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 #include "protos.h"
-#include "elmtNumber.h"
-
 
 int main(int argc, char **argv) {
-  size_t count;
-  int N_images = elmtNumber(count);
 
-  printf("%d\n",N_images );
+    if(argc<2){
+      return printf("Usage : [load pgm] [train pgm] [test pgm]\n" );
+    }
+      ppm_t *p_in = ppm_open(argv[1]);
+        if(!p_in)
+          return printf("Error: cannot open ppm file (%s)\n",argv[1] ), -1;
 
-  pgm_t *p_in = pgm_load(argv[1]);
+      ppm_t *gray_image = rgbengrayscale(p_in);
 
-    if(!p_in)
-      return printf("Error: cannot open pgm file (%s)\n",argv[2] ), -2;
+      ppm_t *ppm_train_images = edge_detected(gray_image);
 
+      trainer(1,ppm_train_images);
 
-  pgm_t *p_out = pgm_create(p_in->h, p_in->w, p_in->t);
-
-  printf("Resolution : %llu Pixels\n",(p_in->h * p_in->w));
-
-  pgm_apply_sobel_filter(p_in->p, p_out->p, p_in->h, p_in->w, 100);
-
-  pgm_save(argv[2], p_out);
-
-  pgm_t *train_images;
-
-  train_images = pgm_load(argv[2]);
-
-  pgm_t *test_images;
-
-  test_images = pgm_load(argv[3]);
-
-  trainer(1,train_images,test_images);
-
-  if(!train_images & !test_images)
-      return printf("Error : cannot open ppm file(%s) (%s)\n", argv[1], argv[2]), -1;
-
-  pgm_close(p_in);
-  pgm_close(p_out);
+      ppm_close(p_in);
+      ppm_close(gray_image);
+      ppm_close(ppm_train_images);
 
   return 0;
 }
