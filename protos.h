1ac4383dc733f4a51fa97b722eda3d891d82f0af
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2020-04-06 12:33:05 +0200

    Appel des fonction du CNN

--- protos.h
+++ protos.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <cmocka.h>
 #define max(a, b) ((a) > (b)) ? (a) : (b)
 
 #define INDEX(x, y, w) (((x) * (w)) + (y))
@@ -11,41 +12,40 @@ typedef unsigned u32;
 typedef unsigned char byte;
 typedef unsigned long long u64;
 
-typedef struct pgm_s{
+typedef struct ppm_s{
   u64 w;
   u64 h;
   u64 t;
-  byte *p;
-}pgm_t;
+  byte *px;
+}ppm_t;
 
 typedef struct pair{
   float a[100];
   float b[100][100];
 }pair;
 
-pgm_t *rgbengrayscale(pgm_t *train_images);
-
-u64 relu(u32 x);
-
 float d_sigmoid(float x);
 
 float sigmoidbis(float x);
 
-pgm_t *pgm_load(char *fname);
+ppm_t *ppm_open(char *fname);
 
-void pgm_save(char *fname, pgm_t *p);
+ppm_t *rgbengrayscale(ppm_t *train_images);
 
-pgm_t *pgm_create(u64 h, u64 w, u64 t);
+ppm_t *ppm_create(u64 h, u64 w, u64 t);
+
+void ppm_save(char *fname, ppm_t *p);
 
 int convolve(byte *m, u64 mh, u64 mw, int *f, u64 fh, u64 fw);
 
-void pgm_apply_sobel_filter(byte *img_in, byte *img_out, u64 h, u64 w, float threshold);
+void ppm_apply_sobel_filter(byte *img_in, byte *img_out, u64 h, u64 w, float threshold);
 
-void pgm_close(pgm_t *p);
+ppm_t *edge_detected(ppm_t *img_bord);
 
-void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image);
+void ppm_close(ppm_t *p);
 
-void testing(float w[][100], int n_w, int n_h, pgm_t *pp_images, float *h);
+void trainer(int nn, ppm_t *pp_train_images);
 
+void testing(float w[][100], int n_w, int n_h, ppm_t *pp_images, float *h);
 
 #endif
