2955752a38c67e0003b2a69775636fb7dcb1046a
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2020-04-06 12:32:30 +0200

    fonction du CNN

--- nn.c
+++ nn.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmocka.h>
 
 //Fonction d_sigmoid
 inline float sigmoidbis(float x)
@@ -17,97 +18,18 @@ float d_sigmoid(float x)
 }
 
 /*Fonction pour convertir les imange en grayscale*/
-pgm_t *rgbengrayscale( pgm_t *train_images){
+ppm_t *rgbengrayscale( ppm_t *train_images){
 
   for(int i=0; i < train_images->w * train_images->h * 3; i +=3)
     {
 
-      byte gray = train_images->p[i+2];
+      byte gray = train_images->px[i+2];
 
-      train_images->p[i]     = gray;
-      train_images->p[i + 1] = gray;
-      train_images->p[i + 2] = gray;
+      train_images->px[i]     = gray;
+      train_images->px[i + 1] = gray;
+      train_images->px[i + 2] = gray;
   }
   return train_images;
-}
-
-/*Fonction de rectification lineaire :*/
-inline u64 relu(u32 x){
-  u64 res =max(0,x);
-  return res;
-}
-
-/*Fonction pour ouvrir les images sous format pgm*/
-pgm_t *pgm_load(char *fname){
-  char c1, c2;
-  int N_images;
-  pgm_t *p = NULL;
-  for(int i = 0; i < N_images; i++)
-  {
-    FILE *fd = fopen(fname, "rb");
-
-    if(fd){
-
-      p = malloc(sizeof(pgm_t));
-
-      fscanf(fd, "%c%c", &c1, &c2);
-
-      fscanf(fd, "%llu %llu\n", &p->w, &p->h);
-
-      fscanf(fd,"%llu\n", &p->t);
-
-      p->p = malloc(sizeof(byte) * p->h * p->w);
-
-      fread(p->p, sizeof(byte), p->h *p->w, fd);
-
-      fclose(fd);
-    }
-  }
-
-  return p;
-}
-
-/*Fonction pour sauvegarder les images pgm*/
-void pgm_save(char *fname, pgm_t *p){
-
-  FILE *fd = fopen(fname, "wb");
-
-  if(fd){
-
-    fprintf(fd, "P5\n");
-
-    fprintf(fd, "%llu %llu\n", p->w, p->h );
-
-    fprintf(fd, "%llu\n", p->t);
-
-    fwrite(p->p, sizeof(byte), p->h *p->w, fd);
-
-    fclose(fd);
-  }
-}
-
-void pgm_close(pgm_t *p){
-
-  if(p){
-    if (p->p)
-      free(p->p);
-
-    free(p);
-  }
-}
-
-/*onction pour cree une image de sortie*/
-pgm_t *pgm_create(u64 h, u64 w, u64 t){
-
-  pgm_t *p = malloc( sizeof(pgm_t));
-
-  p->h = h;
-  p->w = w;
-  p->t = t;
-
-  p->p =malloc(sizeof(byte)*w*h);
-
-  return p;
 }
 
 /*Convolution de 2 matrices*/
@@ -120,9 +42,90 @@ int convolve(byte *m, u64 mh, u64 mw, int *f, u64 fh, u64 fw){
 
   return r;
 }
+/*Fonction pour enregistre l'image en ppm*/
+ppm_t *ppm_open(char *fname){
+
+  char c0, c1, c;
+  FILE *fd = fopen(fname, "rb");
+
+  if(fd){
+    ppm_t *p = malloc(sizeof(ppm_t));
+
+    fscanf(fd,"%c%c\n", &c0, &c1);
+
+    c = fgetc(fd);
+
+    if(c == '#'){
+
+      while(c != '\n')
+          c = fgetc(fd);
+    }
+    else
+        fseek(fd, -1, SEEK_CUR);
+
+    fscanf(fd, "%lld %lld\n", &p->w, &p->h);
+    fscanf(fd, "%lld\n", &p->t);
+
+    p->px = malloc(sizeof(byte) * p->w * p->h * 3);
+
+    if((c0 == 'p') & (c1 == '6'))
+          {
+            fread(p->px, sizeof(byte), p->w * p->h *3, fd);
+          }
+    else
+        if((c0 == 'P') & (c1 == '3'))
+      {
+      }
+
+      fclose(fd);
+
+      return p;
+  }
+  else
+      return NULL;
+}
+
+/*Fonction sauvegarder les images en ppm*/
+void ppm_save(char *fname, ppm_t *p){
+
+  FILE * fd = fopen(fname, "wb");
+
+  fprintf(fd, "P6\n");
+  fprintf(fd, "%lld %lld\n", p->w, p->h);
+  fprintf(fd, "%d\n", 255);
+
+  fwrite(p->px, sizeof(byte), p->w * 3 * p->h, fd);
+
+  fclose(fd);
+}
+
+//
+void ppm_close(ppm_t *p){
+
+  if(p){
+
+    if(p->px)
+      free(p->px);
+
+    free(p);
+  }
+}
+/*Fonction pour créer une image ppm des sortie*/
+ppm_t *ppm_create(u64 h, u64 w, u64 t){
+
+  ppm_t *p = malloc(sizeof(ppm_t));
+
+  p->h = h;
+  p->w = w;
+  p->t = t;
+
+  p->px = malloc(sizeof(byte) * w *h);
+
+  return p;
+}
 
 /*fonction pour l'application du filtre de sobell*/
-void pgm_apply_sobel_filter(byte *img_in, byte *img_out, u64 h, u64 w, float threshold){
+void ppm_apply_sobel_filter(byte *img_in, byte *img_out, u64 h, u64 w, float threshold){
 
   int gx, gy;
   int f1[9] = { -1, 0, 1,
@@ -144,7 +147,22 @@ void pgm_apply_sobel_filter(byte *img_in, byte *img_out, u64 h, u64 w, float thr
     }
 }
 
-void testing(float w[][100], int n_w, int n_h, pgm_t *pp_images, float *h){
+/*foction pour detecter le bord*/
+
+ppm_t *edge_detected(ppm_t *img_bord){
+
+  ppm_t *p_out = ppm_create(img_bord->h, img_bord->w, img_bord->t);
+
+  printf("Resolution : %llu Pixels\n",(img_bord->h * img_bord->w));
+
+  ppm_apply_sobel_filter(img_bord->px, p_out->px, img_bord->h, img_bord->w, 100);
+
+  ppm_close(p_out);
+
+  return p_out;
+}
+/*Fonction pour faire le test*/
+void testing(float w[][100], int n_w, int n_h, ppm_t *pp_images, float *h){
 
   float s, _s,l[100];
 
@@ -153,7 +171,7 @@ void testing(float w[][100], int n_w, int n_h, pgm_t *pp_images, float *h){
     s = 0.0;
 
     for(int j = 1; j <= n_w; j++){
-        s += pp_images->p[j] * w[j][i];
+        s += pp_images->px[j] * w[j][i];
         l[i] = sigmoidbis(s);
       }
   }
@@ -166,17 +184,17 @@ void testing(float w[][100], int n_w, int n_h, pgm_t *pp_images, float *h){
     _s = sigmoidbis(s);
 
     /*Sortie: Detection de cancer*/
-    char *output1 = "Cancer not detected\n";
-    char *output2 = "Cancer detected\n";
+    char *output1 = "Cancer detected\n";
+    char *output2 = "Cancer not detected\n";
 
-    if(nearbyint(_s) > 0.7){
+    if((_s) < 0.7){
       printf("Result : %s\n", output1);
     }
     else{
       printf("Result : %s\n", output2);
     }
 
-    printf("\n Probabilite : (%lf) \n\n Press enter to continue ...",nearbyint(_s));
+    printf("\n Probabilite : (%lf)  : %lf\n\n Press enter to continue ...\n",_s,nearbyint(_s));
 
     getchar();
 
@@ -184,7 +202,7 @@ void testing(float w[][100], int n_w, int n_h, pgm_t *pp_images, float *h){
 }
 
 /*Fonction de train qui renvoie les poids du reseau*/
-void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image){
+void trainer(int nn, ppm_t *pp_train_images){
 
   int n_w = 2500, n_h = 100;
 
@@ -209,9 +227,8 @@ void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image){
         for(int i = 1; i <= n_h; i++){
           s = 0.0;
             for(int j = 1; j <= n_w; j++){
-              s += pp_train_images->p[j] * w0[j][i];
+              s += pp_train_images->px[j] * w0[j][i];
             }
-
               lw0[k][i] = sigmoidbis(s);
         }
     }
@@ -244,10 +261,9 @@ void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image){
           s=0.0;
 
           for(int l = 1; l <= nn; l++){
-            s += (pp_train_images->p[l] * lw_d[l][k]);
+            s += (pp_train_images->px[l] * lw_d[l][k]);
           }
             w0[j][k] -= (alpha *s);
-
         }
       }
 
@@ -276,7 +292,7 @@ void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image){
 
          printf("Retrains : %d, erreur : %lf\n",retrains, err_n );
 
-         printf("Retrain (0), Keep training (1), or test (2): \n" );
+         printf("Retrain (0), Keep training (1), Exit(2) \n" );
          mode = getchar();
 
          if (mode == '0')
@@ -285,15 +301,9 @@ void trainer(int nn, pgm_t *pp_train_images, pgm_t *test_image){
          if (mode == '1')
             goto lbl2;
          else
-          if (mode == '2')
-              testing(w0, n_w, n_h, test_image, h);
-      }
+         if (mode == '2')
+               exit(0);
+
     }
   }
-
-  /*Save the result of training*/
-
-void save()
-{
-
 }
