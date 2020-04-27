d994c77bcec1d1cca24d6df8431cc2215328470f
Nambinina13 <hery.andrianantenaina@ens.uvsq.fr>
2020-04-28 00:43:14 +0200

    main du CNN

--- main.c
+++ main.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <dirent.h>
+#include <fcntl.h>
 #include "protos.h"
 
 int main(int argc, char **argv) {
 
+char *path;
+ppm_t *p_in;
+ppm_t *gray_image;
+ppm_t *ppm_train_images;
+int nb_files;
+char buf[512];
+struct dirent **list =NULL;
+
     if(argc<2){
       return printf("Usage : [load pgm] [train pgm] [test pgm]\n" );
     }
-      ppm_t *p_in = ppm_open(argv[1]);
-        if(!p_in)
-          return printf("Error: cannot open ppm file (%s)\n",argv[1] ), -1;
 
-      ppm_t *gray_image = rgbengrayscale(p_in);
+    path = argv[1];
+    nb_files =scandir(path, &list, NULL, alphasort );
 
-      ppm_t *ppm_train_images = edge_detected(gray_image);
+    if(nb_files<0 )
+    {
+      perror("scandir");
+    }
 
+    for (int i = 2; i < nb_files; i++) {
+
+      snprintf(buf, 512, "%s%s", path, list[i]->d_name);
+      p_in = ppm_open(buf);
+      gray_image = rgbengrayscale(p_in);
+      ppm_train_images = edge_detected(gray_image);
       trainer(1,ppm_train_images);
+    }
+
 
       ppm_close(p_in);
       ppm_close(gray_image);
